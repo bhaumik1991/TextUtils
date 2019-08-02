0e6f6215c46a0440e48c89d48b051772763b0881
commonsub <commonsub816@gmail.com>
2019-08-02 16:59:53 +0530

    Remove Unnessaary lines
    Text Utils using Django

--- Textutils/views.py
+++ Textutils/views.py
@@ -5,14 +5,12 @@ def home(request):
     return render(request,'index.html')
 
 def analyze(request):
-    #Get the text
     djtext = request.POST.get('text','default')
     removepunc = request.POST.get('removepunc','default')
     fullcaps = request.POST.get('fullcaps','default')
     newlineremover = request.POST.get('newlineremover','default')
     spaceremover = request.POST.get('spaceremover','default')
-    # charcount = request.POST.get('charcount','default')
-    # analyzed = djtext
+
     if removepunc == "on":
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed = ""
@@ -21,7 +19,6 @@ def analyze(request):
                 analyzed = analyzed + char
         params = {'purpose':'Removed Punctuations', 'analyzed_text':analyzed}
         djtext = analyzed
-        # return render(request, 'analyze.html', params)
 
     if fullcaps == "on":
         analyzed = ""
@@ -29,7 +26,6 @@ def analyze(request):
             analyzed = analyzed + char.upper()
         params = {'purpose':'Change to uppercase', 'analyzed_text':analyzed}
         djtext = analyzed
-        # return render(request, 'analyze.html', params)
 
     if newlineremover == "on":
         analyzed = ""
@@ -38,7 +34,6 @@ def analyze(request):
                 analyzed = analyzed + char
         params = {'purpose':'Removed New Lines', 'analyzed_text':analyzed}
         djtext = analyzed
-        # return render(request, 'analyze.html', params)
 
     if spaceremover == "on":
         analyzed = ""
@@ -47,32 +42,13 @@ def analyze(request):
                 analyzed = analyzed +char
         params = {'purpose': 'Space Remover', 'analyzed_text': analyzed}
 
-    # if charcount == "on":
-    #     analyzed =len(djtext)
-    #     params = {'purpose': 'Character Counter', 'analyzed_text': analyzed}
-
     if (removepunc != "on" and fullcaps != "on" and newlineremover != "on" and spaceremover != "on"):
         return HttpResponse("Please select any of the choices!")
 
     return render(request, 'analyze.html', params)
-
-    # else:
-    #     return HttpResponse("Error")
 
 def about(request):
     return render(request,'about.html')
 
 def contact(request):
     return render(request,'contact.html')
-
-# def capfirst(request):
-#     return HttpResponse("capitalize first")
-#
-# def newlineremove(request):
-#     return HttpResponse("newline remove")
-#
-# def spaceremove(request):
-#     return HttpResponse("space remove")
-#
-# def charcount(request):
-#     return HttpResponse("character count")
